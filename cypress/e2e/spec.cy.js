76d76e2617584b9cf0c635f3166c23d8f0a5a9ac
Ann Hochworter <ann.hochworter@gmail.com>
2023-10-30 11:50:13 -0600

    Add Cypress testing for the home page elements

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,35 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe('should load the home page', () => {
+  beforeEach(() => {
+    cy.intercept('GET', 'http://localhost:3001/api/v1/tricks', {
+      statusCode: 200,
+      fixture: 'trickData',
+    })
+      .as('getTricks')
+      .visit('http://localhost:3000');
+    cy.wait('@getTricks');
+  });
+
+  it('should load home page elements', () => {
+    cy.get('h1')
+      .contains('Sick Trick Wish List')
+      .get('.card')
+      .first()
+      .contains('h2', 'regular treflip')
+      .get('.card')
+      .last()
+      .contains('h2', 'regular frontside 50-50, backside 180 out');
+  });
+
+  it('should have form fields to fill out', () => {
+    cy.get("input[name='trick-name']")
+      .type('high-five')
+      .get("select[name='select-stance']")
+      .select('Regular')
+      .get("select[name='select-obstacle']")
+      .select('Pool')
+      .get("input[name='tutorial']")
+      .type('https://www.hp.com')
+      .get('.submit-btn')
+      .should('exist');
+  });
+});
